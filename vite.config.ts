b4baabc471dfa1effa13a4c9171fafc149ef24cf
Игорь Миргород <Mirgorod.IA@omsk.gazprom-neft.ru>
2024-07-10 00:56:26 +0600

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import rollupReplace from '@rollup/plugin-replace'
 import * as path from 'path'
+import { env } from 'node:process'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: env.NODE_ENV == 'development' ? undefined : '/zoloto55',
     plugins: [
         rollupReplace({
             preventAssignment: true,
